351509322d14616e673508e2be7255a7f13c354a
akira <akira>
2002-09-25 12:50:26 +0000

    moved test/test3.rb to examples/secret_input1.rb

--- examples/secret_input1.rb
+++ examples/secret_input1.rb
@@ -1,10 +1,10 @@
-# shadows input then reports [ruby-list:15968]
+# to input secretly [ruby-list:15968]
 require 'termios'
 include Termios
 
 oldt = getattr($stdin)
 newt = oldt.dup
-newt.c_lflag &= ~ECHO
+newt.lflag &= ~ECHO
 setattr($stdin, TCSANOW, newt)
 print "noecho> "
 a = $stdin.gets
