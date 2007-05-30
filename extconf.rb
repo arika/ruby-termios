6b40d0f3979b18998ffbb475e3cea4c3e9418bb5
akira <akira>
2007-05-30 04:24:12 +0000

    rb_io_t test isnot needed for ruby1.6.

--- extconf.rb
+++ extconf.rb
@@ -3,7 +3,7 @@ require 'mkmf'
 if have_header('termios.h') &&
     have_header('unistd.h')
 
-  unless have_type("rb_io_t", ["ruby.h", "rubyio.h"])
+  unless RUBY_VERSION < '1.7' || have_type("rb_io_t", ["ruby.h", "rubyio.h"])
     have_struct_member("OpenFile", "fd", ["ruby.h", "rubyio.h"])
   end
 
