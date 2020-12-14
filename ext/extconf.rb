3144cd472e80726fe0908daec2f5e60f6bfd2a08
Aaron Patterson <tenderlove@ruby-lang.org>
2020-12-14 14:24:28 -0800

    fix build on Ruby 3

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -9,14 +9,18 @@ if have_header('termios.h') &&
   have_header('sys/ioctl.h')
 
   if RUBY_VERSION >= '1.7'
-    have_header('ruby/io.h')
-    if have_type("rb_io_t", ["ruby.h", "rubyio.h"])
-      have_struct_member("rb_io_t", "fd", ["ruby.h", "rubyio.h"])
+    if have_header('ruby/io.h')
+      have_type("rb_io_t", ["ruby/io.h"])
+      have_struct_member("rb_io_t", "fd", ["ruby/io.h"])
     else
-      have_struct_member("OpenFile", "fd", ["ruby.h", "rubyio.h"])
-    end
-    if have_macro("OpenFile", ["ruby.h", "rubyio.h"])
-      $defs.push("-DHAVE_MACRO_OPENFILE")
+      if have_type("rb_io_t", ["ruby.h", "rubyio.h"])
+        have_struct_member("rb_io_t", "fd", ["ruby.h", "rubyio.h"])
+      else
+        have_struct_member("OpenFile", "fd", ["ruby.h", "rubyio.h"])
+      end
+      if have_macro("OpenFile", ["ruby.h", "rubyio.h"])
+        $defs.push("-DHAVE_MACRO_OPENFILE")
+      end
     end
   end
 
